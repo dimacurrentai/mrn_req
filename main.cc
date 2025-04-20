4c830887ab1d6690f5b2953e990916d20c045331
Dima Korolev <dima@current.ai>
2025-04-20 07:06:38 -0700

    Divisors chunked response.

--- main.cc
+++ main.cc
@@ -9,19 +9,28 @@ DEFINE_uint16(port, 7001, "The port to run on.");
 
 int main() {
   auto& server = HTTP(current::net::BarePort(FLAGS_port));
-  auto scope = server.Register("/", [](Request r) {
-    r("Hello, World!\n");
-  });
-  scope += server.Register("/chunked", [](Request r) {
-    std::thread([](Request r) {
-      auto c = r.SendChunkedResponse();
-      int const n = 10;
-      for (int i = 0; i < n; ++i) {
-        c(current::strings::Printf("Chunk %d/%d\n", i + 1, n));
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-      }
-      c("Done!\n");
-    }, std::move(r)).detach();
+  auto ok = [](Request r) { r("OK, try /100\n"); };
+  auto scope = server.Register("/", ok);
+  scope += server.Register("/ok", ok);
+  scope += server.Register("/status", ok);
+  scope += server.Register("/", URLPathArgs::CountMask::One, [](Request r) {
+    int n;
+    if (!(std::istringstream(r.url_path_args[0]) >> n)) {
+      r("Need a number as a URL path arg.\n");
+    } else if (!(n >= 1 && n <= 1000)) {
+      r("Need a number between 1 and 1000 as a URL path arg.\n");
+    } else {
+      std::thread([n](Request r) {
+        auto c = r.SendChunkedResponse();
+        for (int i = n; i >= 1; --i) {
+          if ((n % i) == 0) {
+            c(current::strings::Printf("RESPONSE|local|A divisor of %d is %d.\n", n, i));
+            std::this_thread::sleep_for(std::chrono::milliseconds(i * 10));
+          }
+        }
+        c(current::strings::Printf("RESPONSE|local|Done for %d!\n", n));
+      }, std::move(r)).detach();
+    }
   });
   server.Join();
 }
